076554408105bab732219dc2697727b81f7d4efd
Emma Reed <ereed5@uoregon.edu>
2020-05-26 23:02:44 -0700

    JavaScript Class Types: The Animal Class

--- client/javascripts/Animal.js
+++ client/javascripts/Animal.js
@@ -1,4 +1,4 @@
-// jshint esversion: 6
+// jshint esversion: 7
 
 
 //Class Types: Basic syntax
@@ -47,8 +47,23 @@ class Rabbit extends Animal {
 
 // Inherit from Animal
 class Dog extends Animal {
-  //your code goes here..
+  constructor(name, breed, weight) {
+    super(name);
+    this.breed = breed;
+    this.weight = weight;
+  }
 
+  //override the inherited method
+  stop() {
+    super.stop(); // call parent stop
+
+  }
+  bark1() {
+    return (`Woof Woof!`);
+  }
+  bark2() {
+    return (`Yip Yip`);
+  }
 }
 
 let test_driver = () => {
@@ -60,21 +75,23 @@ let test_driver = () => {
     rabbit.stop();
     console.log(rabbit.name);
     console.log(rabbit.earLength);
-  }
+  };
 
   let test_dog = () => {
     let fang = new Dog("Fang", "Airedale", 50, "Woof");
     fang.run(20);
     console.log(`${fang.name} is a ${fang.breed} dog weighing ${fang.weight} lbs. that can run ${fang.speed} mph.`);
-    console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+    console.log(`Look, a cat! ${fang.name} barks: ${fang.bark1()}`);
     fang.stop();
-    const  zero = new Dog("Zero", "Beagle", 22, "Yip" );
+    console.log(fang.bark1());
+    let zero = new Dog("Zero", "Beagle", 22, "Yip" );
     zero.run(10);
     console.log(`${zero.name} is a ${zero.breed} dog weighing ${zero.weight} lbs. that can run ${zero.speed} mph.`);
-    console.log(`Look, a cat! ${zero.name} barks: ${zero.bark()}`);
+    console.log(`Look, a cat! ${zero.name} barks: ${zero.bark2()}`);
     zero.stop();
+    console.log(zero.bark2());
 
-  }
+  };
 
   //run tests
   test_rabbit();
